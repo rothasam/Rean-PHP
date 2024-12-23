e33f92e5ab5f431f7fff3194d84cf9c70eb9d5d9
Rotha Sam <samrotha98@gmail.com>
2024-12-23 19:28:16 +0700

    summary function

--- Practice/lesson.test/build-in-func.php
+++ Practice/lesson.test/build-in-func.php
@@ -91,6 +91,52 @@ $num2 = -12.77;
 // echo abs($num2);  // convert to positive
 // echo pi(); // 3.1415926535898
 
+
+
+// let summary those function in math
+// 1. pow() : calculate power of a number
+// 2. sqrt() : calculate square root of a number
+// 3. round() : round a number to nearest integer or specified number of decimals
+// 4. ceil() : round up to nearest integer
+// 5. floor() : round down to nearest integer
+// 6. abs() : convert to positive number
+// 7. pi() : get value of pi
+
+//let summary function in string
+// 1. strrev() : reverse string
+// 2. strlen() : get length of string
+// 3. strpos() : find position of string
+// 4. str_split() : split string into array
+// 5. str_replace() : replace string with another string
+// 6. substr() : get part of string
+// 7. trim() : remove space from string
+// 8. strtolower() : convert string to lower case
+// 9. strtoupper() : convert string to upper case
+// 10. ucfirst() : convert first character to upper case
+// 11. ucwords() : convert first character of each word to upper case
+// 12. str_word_count() : count number of words in string
+
+
+// let summary function in array
+// 1. array_push() : add element to end of array
+// 2. array_pop() : remove last element from array
+// 3. array_shift() : remove first element from array
+// 4. array_unshift() : add element to beginning of array
+// 5. array_merge() : merge two arrays
+// 6. array_merge_recursive() : merge two arrays recursively
+// 7. array_keys() : get keys of array
+// 8. array_values() : get values of array
+// 9. array_count_values() : count number of each value in array
+// 10. array_sum() : sum all values in array
+// 11. array_unique() : remove duplicate values from array
+// 12. array_search() : find value in array
+// 13. in_array() : check if value is in array
+// 14. array_rand() : get random key from array
+
+
+
+
+
  
 
 
