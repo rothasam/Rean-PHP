ba5f1f3d4fb70147b5a85d1b91e7917bfe91b81a
Rotha Sam <samrotha98@gmail.com>
2024-12-23 15:15:05 +0700

    test build-in function

---
+++ Practice/lesson.test/build-in-func.php
@@ -0,0 +1,81 @@
+<?php
+
+// ARRAY
+
+// array_push()
+// array_sum()
+// max(), min()
+// in_array()
+// count()
+// array_column()
+// sort()
+// array_reverse()
+
+// $arr = [5, 3, 8, 1, 9];
+
+
+// STRING
+
+// $arr = 'hello';
+// $str = 'teacher';
+// // echo $str . $arr;
+// // echo strlen($arr);
+// // print_r($arr[0]);
+
+// $name = '   haha jjsidjfsfjs   ';
+// echo strlen($name); 
+// echo '</br>';
+// echo strlen(trim($name));  // cut space around text
+// echo strtolower($name);
+// echo strtoupper($name);
+
+// $Name = 'Sam rotha';
+// echo str_contains($Name,'sam'); // nothing return 
+// echo str_contains($Name,'Sam'); // return 1
+// echo str_contains(strtolower($Name),'sam'); // return 1
+// echo str_starts_with($Name,'rotha'); // nothing
+// echo str_starts_with($Name,'Sam'); // return 1
+// echo str_ends_with($Name,'rotha'); // return 1
+
+// $str = 'hello world';
+// echo str_replace('hello','bye',$str); 
+// echo $str; // still hello world
+// echo substr($str,5); // remove first 5 characters
+
+
+// $color = ['Black','Pink','Yellow',];
+// echo implode(',',$color);  // convert array to string
+// echo implode('/',$color);  //  Output : Black/Pink/Yellow
+
+
+// $str = 'Chan Rotha, Chan Naritha, Chan Chanthea';
+// print_r($str);  // Chan Rotha, Chan Naritha, Chan Chanthea
+// $newArr = explode(',',$str);  // string to array
+// print_r($newArr);  // Array ( [0] => Chan Rotha [1] => Chan Naritha [2] => Chan Chanthea )
+
+
+// $arr = [
+//     ['id' => 1 , 'name' => 'Chan Rotha'],
+//     ['id' => 2 , 'name' => 'Chan Oda'],
+//     ['id' => 3 , 'name' => 'Chan Mara'],
+//     ['id' => 4 , 'name' => 'Chan Dalen']
+// ];
+// Expected result : Chan Rotha, Chan Oda, Chan Mara, Chan Dalen
+
+// #1
+// $result = array_map(function($item){
+//     return $item['name'];
+// },$arr);
+// echo implode(',',$result); // Chan Rotha, Chan Oda, Chan Mara, Chan Dalen
+
+// #2
+// echo 'Name : ' . implode(',',array_column($arr,'name'));
+
+
+
+// MATH 
+
+
+
+
+
