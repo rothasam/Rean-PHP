4568acca2cbc38f163341aa4a4545df2a2a502b6
Rotha Sam <samrotha98@gmail.com>
2025-01-08 13:13:05 +0700

    add header to index.php in teacher in order to intergrate api

--- api-school-ms/api/teachers/index.php
+++ api-school-ms/api/teachers/index.php
@@ -2,6 +2,10 @@
 
 require_once '../../model/Teacher.php';
 header('Content-Type: application/json');
+// this is the error occurred in the console since we haven't include header yet
+// Access to XMLHttpRequest at 'http://api-school-ms.test/api/teachers/index.php' from origin 'http://localhost:5174' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+header('Access-Control-Allow-Origin: *');
+
 
 $teacher = new Teacher();
 echo $teacher->index(); 
