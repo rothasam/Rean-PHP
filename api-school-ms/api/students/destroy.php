864b97b4f4ea95f780b4d505a00699f46e4e6159
Rotha Sam <samrotha98@gmail.com>
2025-01-15 14:08:42 +0700

    add header

--- api-school-ms/api/students/destroy.php
+++ api-school-ms/api/students/destroy.php
@@ -3,6 +3,7 @@
 require_once '../../model/Student.php';
 
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
 
 $student = new Student();
 $student->id = $_GET['id'];
--- api-school-ms/api/students/update.php
+++ api-school-ms/api/students/update.php
@@ -2,6 +2,7 @@
 
 require_once '../../model/Student.php';
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
 
 $student = new Student();
 
