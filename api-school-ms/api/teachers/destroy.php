ff2d34ef9416c57e6a1c89a6867def57637c1b21
Rotha Sam <samrotha98@gmail.com>
2025-01-16 12:26:55 +0700

    add header to teacher file

--- api-school-ms/api/teachers/destroy.php
+++ api-school-ms/api/teachers/destroy.php
@@ -2,6 +2,7 @@
 
 require_once '../../model/Teacher.php';
 header('Content-Type:application/json');
+header('Access-Control-Allow-Origin: *');
 
 $teacher = new Teacher();
 $teacher->id = $_GET['id'];
--- api-school-ms/api/teachers/store.php
+++ api-school-ms/api/teachers/store.php
@@ -3,6 +3,7 @@
 require_once '../../model/Teacher.php';
 
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
 
 $teacher = new Teacher();
 
